74a2386873d228457e96f7fed7de3591107993ed
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:37:33 -0600

    Clase entidad para tener los datos de la direccion del alumno.

--- src/main/java/com/upiicsa/ApiSIP/Model/Direccion.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Direccion.java
@@ -1,4 +1,30 @@
 package com.upiicsa.ApiSIP.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "SIP_DIRECCION")
 public class Direccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_DIRECCION")
+    private Integer id;
+
+    @Column(name = "CALLE", length = 100)
+    private String calle;
+
+    @Column(name = "NUMERO", length = 5)
+    private String numero;
+
+    @Column(name = "COLONIA", length = 100)
+    private String colonia;
+
+    @Column(name = "CODIGOP", length = 10)
+    private String codigoPostal;
+
+    @Column(name = "ENTIDADFED", length = 100)
+    private String entidadFed;
 }
