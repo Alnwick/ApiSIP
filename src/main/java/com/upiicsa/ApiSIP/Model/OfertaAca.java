8c3621cde0ae2f41dd3529fe3c808281372fb93e
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:38:23 -0600

    Clase entidad para tener concentrados los datos de plan de estudios, carrera y escuela para el alumno.

--- src/main/java/com/upiicsa/ApiSIP/Model/OfertaAca.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/OfertaAca.java
@@ -1,4 +1,34 @@
 package com.upiicsa.ApiSIP.Model;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.Carrera;
+import com.upiicsa.ApiSIP.Model.Catalogs.Escuela;
+import com.upiicsa.ApiSIP.Model.Catalogs.PlanEst;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "SIP_OFERTA_ACA")
 public class OfertaAca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_OFERTA")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_ESCUELA")
+    private Escuela escuela;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_CARRERA")
+    private Carrera carrera;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_PLAN")
+    private PlanEst planEst;
 }
