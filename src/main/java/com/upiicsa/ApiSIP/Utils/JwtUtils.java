9c979474eecfb5847628d8c2029ecc28a252da7f
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:39:11 -0600

    Clase para las diferentes funciones para el manejo del Jwt Token.

--- src/main/java/com/upiicsa/ApiSIP/Utils/JwtUtils.java
+++ src/main/java/com/upiicsa/ApiSIP/Utils/JwtUtils.java
@@ -1,4 +1,89 @@
 package com.upiicsa.ApiSIP.Utils;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.upiicsa.ApiSIP.Model.TipoUsuario;
+import com.upiicsa.ApiSIP.Model.Usuario;
+import com.upiicsa.ApiSIP.Repository.Token_Restore.DuracionTokenRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtUtils {
+
+    private final DuracionTokenRepository duracionRepository;
+
+    @Value("${api.security.jwt.private.key}")
+    private String privateKey;
+
+    @Value("${api.security.jwt.user.generator}")
+    private String userGenerator;
+
+    public JwtUtils(DuracionTokenRepository duracionRepository) {
+        this.duracionRepository = duracionRepository;
+    }
+
+    //Metodo para crear el token del usuario
+    public String createToken(Authentication authentication) {
+        try {
+            Usuario userPrincipal = (Usuario) authentication.getPrincipal();
+            Algorithm algorithm = Algorithm.HMAC256(privateKey);
+
+            // Obtener los permisos del usuario autenticado
+            String authorities = authentication.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.joining(","));
+
+            // Crear el token para el usuario
+            return JWT.create()
+                    .withIssuer(userGenerator)
+                    .withSubject(userPrincipal.getCorreo())
+                    .withClaim("id", userPrincipal.getId())
+                    .withClaim("authorities", authorities)
+                    .withExpiresAt(generateDateExpiration(userPrincipal.getTipoUsuario()))
+                    .sign(algorithm);
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Error to create JWT Token", exception);
+        }
+    }
+
+    //Metodo para validar el token
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(privateKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(userGenerator)
+                    .build();
+            return verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid o r expired. Not authorized.");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT) {
+        return decodedJWT.getSubject();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    //Metodo para genera la fecha de expiracion del token
+    private Instant generateDateExpiration(TipoUsuario tipoUsuario) {
+
+        return LocalDateTime.now().plusHours(duracionRepository.getDuracionTokenByTipoUsuario(tipoUsuario))
+                .toInstant(ZoneOffset.of("-05:00"));
+    }
 }
