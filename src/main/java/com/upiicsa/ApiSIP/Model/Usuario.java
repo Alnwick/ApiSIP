e3fcb14c4e29ad2701e519b7bdf099aeaf6982ac
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:33:39 -0600

    Clase entidad para el usuario principal del proyecto.

--- src/main/java/com/upiicsa/ApiSIP/Model/Usuario.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Usuario.java
@@ -1,50 +1,105 @@
 package com.upiicsa.ApiSIP.Model;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.Estatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "SIP_USUARIOS")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class UserSIP {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_USUARIO")
-    private Long id;
+    private Integer id;
 
     @Column(name = "NOMBRE", length = 60)
-    private String firstName;
+    private String nombre;
 
     @Column(name = "PATERNO", length = 60)
-    private String lastName;
+    private String paterno;
 
     @Column(name = "MATERNO", length = 60)
-    private String middleName;
+    private String materno;
 
     @Column(name = "CORREO", length = 100, unique = true)
-    private String email;
+    private String correo;
 
     @Column(name = "CONTRASENA", length = 100)
-    private String password;
+    private String contrasena;
 
     @Column(name = "HABILITADO")
-    private Boolean enabled;
+    private Boolean habilitado;
 
     @Column(name = "FECHA_ALTA")
-    private LocalDateTime createdAt;
+    private LocalDateTime fechaAlta;
 
     @Column(name = "FECHA_BAJA")
-    private LocalDateTime deletedAt;
+    private LocalDateTime fechaBaja;
 
     @ManyToOne
-    @JoinColumn(name = "ID_TIPOUSUARIO", referencedColumnName = "ID_TIPOUSUARIO")
-    private TipoUsuario userType;
+    @JoinColumn(name = "ID_TIPOUSUARIO")
+    private TipoUsuario tipoUsuario;
 
-    // Relación con SIP_CESTATUS (Asumida como Status)
     @ManyToOne
-    @JoinColumn(name = "ID_ESTATUS", referencedColumnName = "ID_C_ESTATUS")
+    @JoinColumn(name = "ID_ESTATUS")
     private Estatus estatus;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        //Agregar permisos
+        this.tipoUsuario.getPermisos()
+                .stream()
+                .map(permiso ->  new SimpleGrantedAuthority(permiso.getDescripcion()))
+                .forEach(authorities::add);
+        //Agregar tipo de Usuario
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.tipoUsuario.getDescripcion()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.correo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.habilitado;
+    }
 }
