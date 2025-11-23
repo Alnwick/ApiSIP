66e84c79e79d5b9faaf82702f301fdef5cd0e187
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:41:18 -0600

    Clase para obtener los detalles de un usuario autenticado.

--- src/main/java/com/upiicsa/ApiSIP/Service/UserDetailsServiceImp.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/UserDetailsServiceImp.java
@@ -1,20 +1,23 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Model.Usuario;
+import com.upiicsa.ApiSIP.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UsuarioRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserSIIP user = userRepository.findByEmail(email)
+        Usuario usuario = repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User with email: " + email + " not found."));
-        return user;
+        return usuario;
     }
 }
