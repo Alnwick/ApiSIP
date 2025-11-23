ee70b123140f2578fe73146585bf3e334bb56b0d
Alnwick <Alnwick0809@outlook.com>
2025-11-22 18:32:33 -0600

    Clase entidad para guardar el codigo de confirmacion de correo.

--- src/main/java/com/upiicsa/ApiSIP/Model/Token_Restore/CodigoConfirm.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Token_Restore/CodigoConfirm.java
@@ -1,9 +1,12 @@
-package com.upiicsa.ApiSIP.Model;
+package com.upiicsa.ApiSIP.Model.Token_Restore;
 
+import com.upiicsa.ApiSIP.Model.Usuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -19,5 +22,13 @@ public class CodigoConfirm {
     @Column(name = "CODIGO", length = 10)
     private String codigo;
 
-    @Column()
+    @Column(name = "FECHA_EXPIRACION")
+    private LocalDateTime fechaExpiracion;
+
+    @Column(name = "FECHA_USO")
+    private LocalDateTime fechaUso;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_USUARIO")
+    private Usuario usuario;
 }
