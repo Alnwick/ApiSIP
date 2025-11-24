5d79c90c049fcdaa422013ed93248d2787ba3b95
Alnwick <Alnwick0809@outlook.com>
2025-11-24 15:08:56 -0600

    Clase exception para errores genericos.

--- src/main/java/com/upiicsa/ApiSIP/Exception/ValidationException.java
+++ src/main/java/com/upiicsa/ApiSIP/Exception/ValidationException.java
@@ -1,7 +1,7 @@
 package com.upiicsa.ApiSIP.Exception;
 
 public class ValidationException extends RuntimeException {
-  public ValidationException(String message) {
-    super(message);
-  }
+    public ValidationException(String message) {
+        super(message);
+    }
 }
