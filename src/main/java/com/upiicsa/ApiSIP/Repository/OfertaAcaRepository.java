f41f877c80dd97a5eb885c8d3673bb74bd844bfe
Alnwick <Alnwick0809@outlook.com>
2025-11-24 15:00:22 -0600

    Clase repositorio para tener comunicacion con la bd de ofertas academicas.

--- src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
@@ -1,4 +1,21 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface OfertaAcaRepository {
+import com.upiicsa.ApiSIP.Model.OfertaAca;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface OfertaAcaRepository extends JpaRepository<OfertaAca, Integer> {
+
+    @Query("SELECT o FROM OfertaAca o " +
+            "WHERE o.escuela.nombre = :nombreEscuela " +
+            "AND o.carrera.siglas = :nombreCarrera " +
+            "AND o.planEst.codigo = :codigoPlan")
+    Optional<OfertaAca> findByCompositeKeys(
+            @Param("nombreEscuela") String escuela,
+            @Param("nombreCarrera") String carrera,
+            @Param("codigoPlan") String plan
+    );
 }
