e8207736dcc7f4a5678bc54d5c9ae9bc9d1b1f75
Alnwick <Alnwick0809@outlook.com>
2025-11-23 20:28:26 -0600

    Repositorio para acceder en BD a los codigos de confirmacion de email.

--- src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/CodigoConfirmRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/CodigoConfirmRepository.java
@@ -1,5 +1,13 @@
 package com.upiicsa.ApiSIP.Repository.Token_Restore;
 
-public interface EmailConfTokenRepository {
+import com.upiicsa.ApiSIP.Model.Token_Restore.CodigoConfirm;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface CodigoConfirmRepository extends JpaRepository<CodigoConfirm, Integer> {
+
+    Optional<CodigoConfirm> findByCodigo(String code);
+
+    Optional<CodigoConfirm> findByUsuarioId(Integer usuarioId);
 }
