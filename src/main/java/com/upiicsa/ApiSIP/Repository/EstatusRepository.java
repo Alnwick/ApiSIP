634706e58f062195b3e4db48a3f5ecc33979b838
Alnwick <Alnwick0809@outlook.com>
2025-11-24 15:04:50 -0600

    Clase repositorio para tener comunicacion con la bd de estatus.

--- src/main/java/com/upiicsa/ApiSIP/Repository/EstatusRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/EstatusRepository.java
@@ -1,4 +1,11 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface EstatusRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.Estatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EstatusRepository extends JpaRepository<Estatus, Integer> {
+
+    Optional<Estatus> findByDescripcion(String descripcion);
 }
