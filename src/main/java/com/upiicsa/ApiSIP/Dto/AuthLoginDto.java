db5cd09fbe255e80c4a1ed1463ebd18cca8d2248
Alnwick <Alnwick0809@outlook.com>
2025-11-23 20:26:23 -0600

    Clase dto para datos de login del usuario.

--- src/main/java/com/upiicsa/ApiSIP/Dto/AuthLoginDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/AuthLoginDto.java
@@ -1,4 +1,12 @@
 package com.upiicsa.ApiSIP.Dto;
 
-public record Dto() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthLoginDto(
+        @NotBlank @Email
+        String email,
+        @NotBlank
+        String password
+) {
 }
