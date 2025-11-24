10749704cf457706f3ae050051ffe8487ba1af3b
Alnwick <Alnwick0809@outlook.com>
2025-11-23 20:27:32 -0600

    Clase dto para los datos de confirmacion de email.

--- src/main/java/com/upiicsa/ApiSIP/Dto/Email/EmailConfirmTokenDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Email/EmailConfirmTokenDto.java
@@ -1,4 +1,16 @@
 package com.upiicsa.ApiSIP.Dto.Email;
 
-public record EmailConfirmToken() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EmailConfirmTokenDto(
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Size(min = 6, max = 6)
+        String code
+) {
 }
