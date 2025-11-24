7d3668072b711ed8f9294f919f597a711c1904fc
Alnwick <Alnwick0809@outlook.com>
2025-11-23 20:29:01 -0600

    Repositorio para acceder en BD a los usuarios.

--- src/main/java/com/upiicsa/ApiSIP/Repository/UsuarioRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.upiicsa.ApiSIP.Repository;
 
+import com.upiicsa.ApiSIP.Model.TipoUsuario;
 import com.upiicsa.ApiSIP.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
+
+    Optional<TipoUsuario> findTipoUsuarioByEmail(String email);
 }
