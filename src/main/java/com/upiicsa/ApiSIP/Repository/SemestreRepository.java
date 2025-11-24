5016a0e127262dcc472402c1027e970f6af985d1
Alnwick <Alnwick0809@outlook.com>
2025-11-24 14:59:30 -0600

    Clase repositorio para tener comunicacion con la bd de semestres.

--- src/main/java/com/upiicsa/ApiSIP/Repository/SemestreRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/SemestreRepository.java
@@ -1,4 +1,11 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface SemestreRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.Semestre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SemestreRepository extends JpaRepository<Semestre, Integer> {
+
+    Optional<Semestre> findByDescripcion(String descripcion);
 }
