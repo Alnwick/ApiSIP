cf2b821b1a99491fa1f45a96f9749efd400e3531
Alnwick <Alnwick0809@outlook.com>
2025-11-24 15:07:20 -0600

    Clase dto para el registro de un estudiante.

--- src/main/java/com/upiicsa/ApiSIP/Dto/Student/StudentRegistrationDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Student/StudentRegistrationDto.java
@@ -1,4 +1,18 @@
 package com.upiicsa.ApiSIP.Dto.Student;
 
-public record StudentRegistrationDto() {
+public record StudentRegistrationDto(
+        String correo,
+        String paterno,
+        String materno,
+        String nombre,
+        String contrasena,
+        String confirmarContrasena,
+        String matricula,
+        String telefono,
+        String semestreDes,
+        boolean egresado,
+        String escuelaNom,
+        String carreraNom,
+        String planEstCodigo
+) {
 }
