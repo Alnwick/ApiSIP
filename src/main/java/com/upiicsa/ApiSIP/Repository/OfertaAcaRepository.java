47c496f8a5b410baf828743732fa825f41c1b953
Alnwick <Alnwick0809@outlook.com>
2025-11-25 14:01:22 -0600

    Metodos para obtener las escuelas, las carreras por escuela y los planes de estudio por carrera y escuela.

--- src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
@@ -1,11 +1,15 @@
 package com.upiicsa.ApiSIP.Repository;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.Carrera;
+import com.upiicsa.ApiSIP.Model.Catalogs.Escuela;
+import com.upiicsa.ApiSIP.Model.Catalogs.PlanEst;
 import com.upiicsa.ApiSIP.Model.OfertaAca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +24,13 @@ public interface OfertaAcaRepository extends JpaRepository<OfertaAca, Integer> {
             @Param("nombreCarrera") String carrera,
             @Param("codigoPlan") String plan
     );
+
+    @Query("SELECT DISTINCT o.escuela FROM OfertaAca o")
+    List<Escuela> findAllEscuelasDisponibles();
+
+    @Query("SELECT DISTINCT o.carrera FROM OfertaAca o WHERE o.escuela.id = :idEscuela")
+    List<Carrera> findCarrerasByEscuela(@Param("idEscuela") Integer idEscuela);
+
+    @Query("SELECT DISTINCT o.planEst FROM OfertaAca o WHERE o.escuela.id = :idEscuela AND o.carrera.id = :idCarrera")
+    List<PlanEst> findPlanesByEscuelaAndCarrera(@Param("idEscuela") Integer idEscuela, @Param("idCarrera") Integer idCarrera);
 }
