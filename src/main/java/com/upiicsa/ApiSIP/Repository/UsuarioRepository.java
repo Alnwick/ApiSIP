3056ecc431f6682e24485aad06fe739845ba565e
Alnwick <Alnwick0809@outlook.com>
2025-11-24 21:00:37 -0600

    Se agrego la annotation @Repository, se utiliza @Query y se cambia el nombre del metodo para obtener el correo.

--- src/main/java/com/upiicsa/ApiSIP/Repository/UsuarioRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/UsuarioRepository.java
@@ -3,12 +3,17 @@ package com.upiicsa.ApiSIP.Repository;
 import com.upiicsa.ApiSIP.Model.TipoUsuario;
 import com.upiicsa.ApiSIP.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
-    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByCorreo(String correo);
 
-    Optional<TipoUsuario> findTipoUsuarioByEmail(String email);
+    @Query("SELECT u.tipoUsuario FROM Usuario u WHERE u.correo = :correo")
+    Optional<TipoUsuario> findTipoUsuarioByCorreo(@Param("correo") String correo);
 }
