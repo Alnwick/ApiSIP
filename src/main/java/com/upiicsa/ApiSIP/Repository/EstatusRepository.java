64dd00f3941fd662e8874c134cb21bd917f0b81c
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:51:28 -0600

    Se agrego la annotation @Repository.

--- src/main/java/com/upiicsa/ApiSIP/Repository/EstatusRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/EstatusRepository.java
@@ -2,9 +2,11 @@ package com.upiicsa.ApiSIP.Repository;
 
 import com.upiicsa.ApiSIP.Model.Catalogs.Estatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EstatusRepository extends JpaRepository<Estatus, Integer> {
 
     Optional<Estatus> findByDescripcion(String descripcion);
