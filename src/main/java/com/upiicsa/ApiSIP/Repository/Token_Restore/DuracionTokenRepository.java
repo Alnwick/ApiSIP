030167936295d6720437a75e4962ee5b39aadc19
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:53:45 -0600

    Se agrego la annotation @Repository y se utiliza @Query.

--- src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/DuracionTokenRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/DuracionTokenRepository.java
@@ -3,8 +3,13 @@ package com.upiicsa.ApiSIP.Repository.Token_Restore;
 import com.upiicsa.ApiSIP.Model.TipoUsuario;
 import com.upiicsa.ApiSIP.Model.Token_Restore.DuracionToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DuracionTokenRepository extends JpaRepository<DuracionToken, Integer> {
 
-    Integer getDuracionTokenByTipoUsuario(TipoUsuario tipoUsuario);
+    @Query("SELECT d.duracionHoras FROM DuracionToken d WHERE d.tipoUsuario = :tipoUsuario")
+    Integer getDuracionHorasByTipoUsuario(@Param("tipoUsuario") TipoUsuario tipoUsuario);
 }
