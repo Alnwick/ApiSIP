50fa5f0be40856e5e389adf59d17f96bf0aeb4cd
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:51:50 -0600

    Se agrego la annotation @Repository.

--- src/main/java/com/upiicsa/ApiSIP/Repository/EstudianteRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/EstudianteRepository.java
@@ -2,7 +2,9 @@ package com.upiicsa.ApiSIP.Repository;
 
 import com.upiicsa.ApiSIP.Model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstudianteRepository extends JpaRepository<Alumno, Integer> {
 
 }
