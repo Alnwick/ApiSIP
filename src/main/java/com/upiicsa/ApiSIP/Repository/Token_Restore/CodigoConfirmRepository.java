f44df3adb2e161d7a01638423d532c0dfaebac6a
Alnwick <Alnwick0809@outlook.com>
2025-11-24 21:01:05 -0600

    Se agrego la annotation @Repository.

--- src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/CodigoConfirmRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/CodigoConfirmRepository.java
@@ -2,9 +2,11 @@ package com.upiicsa.ApiSIP.Repository.Token_Restore;
 
 import com.upiicsa.ApiSIP.Model.Token_Restore.CodigoConfirm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CodigoConfirmRepository extends JpaRepository<CodigoConfirm, Integer> {
 
     Optional<CodigoConfirm> findByCodigo(String code);
