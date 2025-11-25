89000fdf11b4b04c56e26c7ad841534a3cdc17c3
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:52:34 -0600

    Se agrego la annotation @Repository.

--- src/main/java/com/upiicsa/ApiSIP/Repository/TipoUsuarioRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/TipoUsuarioRepository.java
@@ -2,9 +2,11 @@ package com.upiicsa.ApiSIP.Repository;
 
 import com.upiicsa.ApiSIP.Model.TipoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TipoUsuarioRepository extends JpaRepository<TipoUsuario, Integer> {
 
     Optional<TipoUsuario> findByDescripcion(String descripcion);
