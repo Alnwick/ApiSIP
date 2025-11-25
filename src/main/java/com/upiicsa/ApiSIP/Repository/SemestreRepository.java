f0cf731a3c7e9dde51d43462734d58c43b507c5d
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:52:09 -0600

    Se agrego la annotation @Repository.

--- src/main/java/com/upiicsa/ApiSIP/Repository/SemestreRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/SemestreRepository.java
@@ -2,9 +2,11 @@ package com.upiicsa.ApiSIP.Repository;
 
 import com.upiicsa.ApiSIP.Model.Catalogs.Semestre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SemestreRepository extends JpaRepository<Semestre, Integer> {
 
     Optional<Semestre> findByDescripcion(String descripcion);
