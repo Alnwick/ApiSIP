1971872c5e888c81b4badffa722dd0348d69db54
Alnwick <Alnwick0809@outlook.com>
2025-11-24 20:54:05 -0600

    Se agrego la annotation @Repository y se utiliza @Query.

--- src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/OfertaAcaRepository.java
@@ -4,9 +4,11 @@ import com.upiicsa.ApiSIP.Model.OfertaAca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface OfertaAcaRepository extends JpaRepository<OfertaAca, Integer> {
 
     @Query("SELECT o FROM OfertaAca o " +
