5c01d0dd72694bcc98a7012254600ca6ca6ca152
Alnwick <Alnwick0809@outlook.com>
2025-12-05 23:00:04 -0600

    Clase para tener la llave primaria compuesta de ReviewDocument.

--- src/main/java/com/upiicsa/ApiSIP/Model/Support/ReviewDocumentId.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Support/ReviewDocumentId.java
@@ -1,9 +1,17 @@
 package com.upiicsa.ApiSIP.Model.Support;
 
-import jakarta.persistence.Embeddable;
-
+import jakarta.persistence.Column;
+import lombok.*;
 import java.io.Serializable;
 
-@Embeddable
-public class ReviweDocumentId implements Serializable {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDocumentId implements Serializable {
+
+    @Column(name = "ID_USUARIO")
+    private Integer idOperative;
+
+    @Column(name = "ID_DOCUMENTO")
+    private Integer idDocument;
 }
