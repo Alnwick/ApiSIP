759ab41c94f2d8b258b6e26c9dcf27564e4438e5
Alnwick <Alnwick0809@outlook.com>
2025-12-05 22:53:57 -0600

    Clase entidad para la carga de documentos.

--- src/main/java/com/upiicsa/ApiSIP/Model/Document.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Document.java
@@ -1,4 +1,36 @@
 package com.upiicsa.ApiSIP.Model;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "SIP_CARGADOC")
 public class Document {
+
+    @Id
+    @Column(name = "ID_DOCUMENTO")
+    private Integer ID;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_ALUMNOPROC")
+    private StudentProcess studentProcess;
+
+    @Column(name = "FECHA_CARGA")
+    private LocalDateTime UploadDate;
+
+    @Column(name = "URL")
+    private String URL;
+
+    @Column(name = "FECHA_BAJA")
+    private LocalDateTime cancellationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_TIPODOC")
+    private DocumentType documentType;
 }
