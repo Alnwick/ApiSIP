c5edb3a751c05e0f89a9bcd50dc3a7917f933efb
Alnwick <Alnwick0809@outlook.com>
2025-12-05 22:53:14 -0600

    Clase entidad para catalogo de estados del proceso.

--- src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/ProcessState.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/ProcessState.java
@@ -1,4 +1,18 @@
 package com.upiicsa.ApiSIP.Model.Catalogs;
 
-public class ProcessState {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "SIP_CESTPROCESO")
+public class ProcessState extends BaseCatalog{
+
+    @Id
+    @Column(name = "ID_ESTPROCESO")
+    private Integer id;
 }
