060c1934ed9a1c2fddf21c45e8155866ae2013c7
Alnwick <Alnwick0809@outlook.com>
2025-12-05 22:43:44 -0600

    Clase abstracta para la base de los catalogos que ocupas descripcion.

--- src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/BaseCatalog.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/BaseCatalog.java
@@ -1,4 +1,15 @@
 package com.upiicsa.ApiSIP.Model.Catalogs;
 
-public class BaseCatalog {
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter @Setter
+public abstract class BaseCatalog {
+
+    @Column(name = "DESCRIPCION", length = 100)
+    private String description;
+
 }
