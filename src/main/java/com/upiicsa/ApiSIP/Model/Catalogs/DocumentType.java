9d799e3e431ca75ca3c43d0213dc5fd91db52f06
Alnwick <Alnwick0809@outlook.com>
2025-12-05 22:52:29 -0600

    Clase entidad para catalogo de tipo de documento.

--- src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/DocumentType.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Catalogs/DocumentType.java
@@ -1,4 +1,18 @@
 package com.upiicsa.ApiSIP.Model.Catalogs;
 
-public class DocumentType {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "SIP_CTIPODOC")
+public class DocumentType extends BaseCatalog{
+
+    @Id
+    @Column(name = "ID_TIPODOC")
+    private Integer id;
 }
