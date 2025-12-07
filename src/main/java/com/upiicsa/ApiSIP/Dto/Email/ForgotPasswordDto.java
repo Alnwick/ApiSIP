e2bff7d5e8aa8444a927adaf6dcc4d6a5d1c62a4
Alnwick <Alnwick0809@outlook.com>
2025-12-07 15:18:25 -0600

    Clase dto para recuperar su contrasena

--- src/main/java/com/upiicsa/ApiSIP/Dto/Email/ForgotPasswordDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Email/ForgotPasswordDto.java
@@ -1,4 +1,10 @@
 package com.upiicsa.ApiSIP.Dto.Email;
 
-public record ForgotPassword() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ForgotPasswordDto(
+        @NotBlank @Email
+        String email
+) {
 }
