df1b28f36113ec1504cb334c59a4ae1ce45ed124
Alnwick <Alnwick0809@outlook.com>
2025-12-07 15:22:20 -0600

    Clase repositorio para gestionar los token de cambio de contrasena y el metodo buscar por token.

--- src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/TokenResetRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/Token_Restore/TokenResetRepository.java
@@ -1,4 +1,13 @@
 package com.upiicsa.ApiSIP.Repository.Token_Restore;
 
-public class TokenResetRepository {
+import com.upiicsa.ApiSIP.Model.Token_Restore.TokenReset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenResetRepository extends JpaRepository<TokenReset, Integer> {
+
+    Optional<TokenReset> findByToken(String token);
 }
