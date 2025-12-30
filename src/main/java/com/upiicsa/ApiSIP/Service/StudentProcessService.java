1e99538cbb3b8ac291053d977444e177ecd3f4e0
Alnwick <Alnwick0809@outlook.com>
2025-12-29 19:32:30 -0600

    Clase service para gestionar el procesos de practicas de alumnos.

--- src/main/java/com/upiicsa/ApiSIP/Service/StudentProcessService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/StudentProcessService.java
@@ -1,4 +1,20 @@
 package com.upiicsa.ApiSIP.Service;
 
-public class ProcessService {
+import com.upiicsa.ApiSIP.Model.Student;
+import com.upiicsa.ApiSIP.Repository.StudentProcessRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentProcessService {
+
+    @Autowired
+    private StudentProcessRepository processRepository;
+
+    @Transactional
+    public void setFirstState(Student student) {
+
+
+    }
 }
