4efd4e930c9b7d30b845fa9dbe5f33cbf24489a6
Alnwick <Alnwick0809@outlook.com>
2025-12-29 19:34:31 -0600

    Clase repositorio para acceder a la BD de proceso.

--- src/main/java/com/upiicsa/ApiSIP/Repository/StudentProcessRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/StudentProcessRepository.java
@@ -1,4 +1,11 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface StudentProcessRepository {
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentProcessRepository extends JpaRepository<StudentProcess, Integer> {
+
+
 }
