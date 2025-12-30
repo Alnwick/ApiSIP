55389299c3572b95a883fd280305e3f88efd28d2
Alnwick <Alnwick0809@outlook.com>
2025-12-30 14:10:06 -0600

    Clase repositorio para la interesaccion con la tabla de documento.

--- src/main/java/com/upiicsa/ApiSIP/Repository/DocumentRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/DocumentRepository.java
@@ -1,4 +1,9 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public class DocumentRepository {
+import com.upiicsa.ApiSIP.Model.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Integer> {
 }
