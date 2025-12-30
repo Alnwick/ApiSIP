8bd7c4f97d09954dc6da2a39740553e104d7853a
Alnwick <Alnwick0809@outlook.com>
2025-12-30 14:10:31 -0600

    Clase service para la gestion de los documentos.

--- src/main/java/com/upiicsa/ApiSIP/Service/DocumentService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/DocumentService.java
@@ -1,4 +1,36 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Model.Document;
+import com.upiicsa.ApiSIP.Repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
 public class DocumentService {
+
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    private final Path fileStorageLocation = Paths.get("./uploads").toAbsolutePath().normalize();
+
+    public DocumentService() throws IOException {
+        try{
+            Files.createDirectories(fileStorageLocation);
+        }catch (Exception e){
+            throw new IOException("Not able to create directory", e);
+        }
+    }
+
+    @Transactional
+    public String saveFile(MultipartFile file, String type, Long id) throws IOException {
+        return "";
+    }
+
 }
