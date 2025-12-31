b79a78362aa0d283f709e30b0367913f3c80ba62
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:59:34 -0600

    Se agregaron lo metodos para guardar un documento con su logica de escenarios, guardar en BD un documento y actualizar un documento si es el caso.

--- src/main/java/com/upiicsa/ApiSIP/Service/DocumentService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/DocumentService.java
@@ -1,16 +1,24 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Exception.ValidationException;
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
 import com.upiicsa.ApiSIP.Model.Document;
+import com.upiicsa.ApiSIP.Model.ReviewDocument;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
 import com.upiicsa.ApiSIP.Repository.DocumentRepository;
+import com.upiicsa.ApiSIP.Repository.DocumentTypeRepository;
+import com.upiicsa.ApiSIP.Repository.ReviewDocumentRepository;
+import com.upiicsa.ApiSIP.Repository.StudentProcessRepository;
+import com.upiicsa.ApiSIP.Utils.AuthHelper;
+import com.upiicsa.ApiSIP.Utils.DocumentNamingUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class DocumentService {
@@ -18,19 +26,70 @@ public class DocumentService {
     @Autowired
     private DocumentRepository documentRepository;
 
-    private final Path fileStorageLocation = Paths.get("./uploads").toAbsolutePath().normalize();
+    @Autowired
+    private DocumentTypeRepository typeRepository;
 
-    public DocumentService() throws IOException {
-        try{
-            Files.createDirectories(fileStorageLocation);
-        }catch (Exception e){
-            throw new IOException("Not able to create directory", e);
+    @Autowired
+    private StudentProcessRepository processRepository;
+
+    @Autowired
+    private ReviewDocumentRepository reviewRepository;
+
+    @Autowired
+    private DocumentNamingUtils documentNaming;
+
+    @Autowired
+    private FileStorageService fileStorage;
+
+    @Transactional
+    public void saveDoc(MultipartFile file, String typeName) throws IOException {
+        Integer userId = AuthHelper.getAuthenticatedUserId();
+        StudentProcess process =  processRepository.findByStudentId(userId)
+                .orElseThrow(()->new IllegalArgumentException("Student not found"));
+
+        DocumentType type = typeRepository.findByDescription(typeName).orElse(null);
+
+        Optional<Document> document = documentRepository.findByStudentProcessAndDocumentTypeAndCancellationDateIsNull
+                (process, type);
+
+        if(document.isPresent()){
+            Document currentDoc = document.get();
+
+            ReviewDocument review = reviewRepository.findFirstByDocumentOrderByIdDesc(currentDoc);
+            if (review != null) {
+                if (review.getStatus().getId() == 2) {
+                    throw new ValidationException("Este documento ya fue aprobado y no puede modificarse.");
+                } else if (review.getStatus().getId() == 3) {
+                    currentDoc.setCancellationDate(LocalDateTime.now());
+                    documentRepository.save(currentDoc);
+                    createNewDocument(process, type, file);
+                }
+            } else{
+                updateDoc(currentDoc, typeName, file);
+            }
+        } else{
+            createNewDocument(process, type, file);
         }
     }
 
-    @Transactional
-    public String saveFile(MultipartFile file, String type, Long id) throws IOException {
-        return "";
+    public void createNewDocument(StudentProcess process, DocumentType type, MultipartFile file){
+        Document newDocument = Document.builder()
+                .studentProcess(process)
+                .UploadDate(LocalDateTime.now())
+                .URL(documentNaming.generateVersionedName(process, type))
+                .documentType(type)
+                .build();
+
+        documentRepository.save(newDocument);
+        fileStorage.store(file, documentNaming.generateVersionedName(process, type));
     }
 
+    public void updateDoc(Document currentDoc, String typeName, MultipartFile file) throws IOException {
+        if(!currentDoc.getDocumentType().getDescription().equals(typeName)){
+            throw new ValidationException("Type for document not coincided.");
+        }
+        currentDoc.setUploadDate(LocalDateTime.now());
+        documentRepository.save(currentDoc);
+        fileStorage.store(file, currentDoc.getURL());
+    }
 }
