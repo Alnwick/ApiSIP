1719553d7ee1c780f372d6ed6c03bb58c5a34222
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:57:39 -0600

    Clase controlador para lo relacionado a documentos.

--- src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
+++ src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
@@ -1,4 +1,27 @@
 package com.upiicsa.ApiSIP.Controller;
 
+import com.upiicsa.ApiSIP.Service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/documents")
 public class DocumentController {
+
+    @Autowired
+    private DocumentService documentService;
+
+    @RequestMapping("/upload")
+    @PreAuthorize("hasAnyRole('STUDENT')")
+    public ResponseEntity<Void> uploadDocument(MultipartFile file, String type) throws IOException {
+        documentService.saveDoc(file, type);
+
+        return ResponseEntity.ok().build();
+    }
 }
