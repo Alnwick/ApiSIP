b422a9e575bcd40175f2f666dc7da569ca17188e
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:56:30 -0600

    Metodo para buscar un documento por su tipo, proceso al que pertenece y si esta cancelado y tambienmetodo para contar los documentos por tipo y proceso.

--- src/main/java/com/upiicsa/ApiSIP/Repository/DocumentRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/DocumentRepository.java
@@ -1,9 +1,18 @@
 package com.upiicsa.ApiSIP.Repository;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
 import com.upiicsa.ApiSIP.Model.Document;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
+
+    Optional<Document> findByStudentProcessAndDocumentTypeAndCancellationDateIsNull
+            (StudentProcess process, DocumentType type);
+
+    Integer countByStudentProcessAndDocumentType(StudentProcess process, DocumentType type);
 }
