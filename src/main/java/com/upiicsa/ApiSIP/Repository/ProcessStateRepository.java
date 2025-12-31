b1c4a409d029326c043416d4b267c92b4c83cfd1
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:54:05 -0600

    Clase repositorio para comunicarse con la tabla cestproceso y metodo para buscar un estado por su descripcion.

--- src/main/java/com/upiicsa/ApiSIP/Repository/ProcessStateRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/ProcessStateRepository.java
@@ -1,4 +1,12 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface ProcessStateRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.ProcessState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProcessStateRepository extends JpaRepository<ProcessState, Integer> {
+    Optional<ProcessState> findByDescription(String description);
 }
