987f98923defb0af3ebe1d777ee40dc5f9577f00
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:50:29 -0600

    Se coloco el metodo para buscar proceso por el ID del alumno.

--- src/main/java/com/upiicsa/ApiSIP/Repository/StudentProcessRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/StudentProcessRepository.java
@@ -4,8 +4,10 @@ import com.upiicsa.ApiSIP.Model.StudentProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentProcessRepository extends JpaRepository<StudentProcess, Integer> {
 
-
+    Optional<StudentProcess> findByStudentId(Integer userId);
 }
