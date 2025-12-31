4b804d61392e5b2b0231af8ff8ce90ef769a8dfb
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:49:10 -0600

    Se desarrollo el metodo para generar el primer estado de proceso.

--- src/main/java/com/upiicsa/ApiSIP/Service/StudentProcessService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/StudentProcessService.java
@@ -1,10 +1,16 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Exception.ResourceNotFoundException;
+import com.upiicsa.ApiSIP.Model.Enum.StateProcessEnum;
 import com.upiicsa.ApiSIP.Model.Student;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import com.upiicsa.ApiSIP.Repository.ProcessStateRepository;
 import com.upiicsa.ApiSIP.Repository.StudentProcessRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class StudentProcessService {
@@ -12,9 +18,22 @@ public class StudentProcessService {
     @Autowired
     private StudentProcessRepository processRepository;
 
+    @Autowired
+    private ProcessStateRepository  processStateRepository;
+
     @Transactional
     public void setFirstState(Student student) {
+        var state = processStateRepository.findByDescription(StateProcessEnum.ACCEPTANCE.getName())
+                        .orElseThrow(()-> new ResourceNotFoundException("State not found"));
 
+        StudentProcess firstProcess = StudentProcess.builder()
+                .StartDate(LocalDateTime.now())
+                .Active(true)
+                .student(student)
+                .processState(state)
+                .observations("")
+                .build();
 
+        processRepository.save(firstProcess);
     }
 }
