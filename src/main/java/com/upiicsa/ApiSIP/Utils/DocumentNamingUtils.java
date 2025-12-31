597cc4ac2556df5de29ac5e48a08a16ad3f9233a
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:47:29 -0600

    Clase util para generar el nombre con versinado del documento.

--- src/main/java/com/upiicsa/ApiSIP/Utils/DocumentNamingUtils.java
+++ src/main/java/com/upiicsa/ApiSIP/Utils/DocumentNamingUtils.java
@@ -1,4 +1,30 @@
 package com.upiicsa.ApiSIP.Utils;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
+import com.upiicsa.ApiSIP.Model.Student;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import com.upiicsa.ApiSIP.Repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DocumentNamingUtils {
+
+    @Autowired
+    private DocumentRepository docRepository;
+
+    public String generateVersionedName(StudentProcess process, DocumentType type) {
+        String cleanTypeName = type.getDescription().replaceAll("\\s+", "");
+
+        Student student = process.getStudent();
+        long currentCount = docRepository.countByStudentProcessAndDocumentType(process, type);
+        int nextVersion = (int) currentCount + 1;
+        String extension = ".pdf";
+
+        return String.format("%s_%s_v%d%s",
+                student.getEnrollment(),
+                cleanTypeName,
+                nextVersion,
+                extension);
+    }
 }
