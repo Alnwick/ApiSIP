216e6ce6e3984d0fce5e66d75a4d6b975fb517ce
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:54:48 -0600

    Clase repositorio para comunicarse con la tabla ctipodoc y buscar un tipo por su descripcion.

--- src/main/java/com/upiicsa/ApiSIP/Repository/DocumentTypeRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/DocumentTypeRepository.java
@@ -1,4 +1,13 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface DocumentTypeRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DocumentTypeRepository extends JpaRepository<DocumentType, Integer> {
+
+    Optional<DocumentType> findByDescription(String description);
 }
