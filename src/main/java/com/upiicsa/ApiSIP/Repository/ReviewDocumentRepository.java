bbd96f12ab9b548e77b2f0fefd7280fa8ddbe686
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:51:49 -0600

    Clase repositorio para comunicarse con la tabla de RevisionDoc y metodo para buscar un documento que ya tenga revision.

--- src/main/java/com/upiicsa/ApiSIP/Repository/ReviewDocumentRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/ReviewDocumentRepository.java
@@ -1,4 +1,12 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public class ReviewDocumentRepository {
+import com.upiicsa.ApiSIP.Model.Document;
+import com.upiicsa.ApiSIP.Model.ReviewDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewDocumentRepository extends JpaRepository<ReviewDocument, Integer> {
+
+    ReviewDocument findFirstByDocumentOrderByIdDesc(Document document);
 }
