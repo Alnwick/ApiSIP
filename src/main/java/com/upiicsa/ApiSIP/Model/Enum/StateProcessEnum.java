beafa9b730766f4e0aa37e7ec15263079267c443
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:58:17 -0600

    Clase Enum para tener un control en lo relacionado con los estados de un proceso.

--- src/main/java/com/upiicsa/ApiSIP/Model/Enum/StateProcessEnum.java
+++ src/main/java/com/upiicsa/ApiSIP/Model/Enum/StateProcessEnum.java
@@ -1,4 +1,28 @@
 package com.upiicsa.ApiSIP.Model.Enum;
 
+import lombok.Getter;
+
 public enum StateProcessEnum {
+    REGISTERED(1, "Registrado", -1, 2),
+    INITIAL_DOC(2, "Doc Inicial", 1, 3),
+    ACCEPTANCE(3, "Carta Aceptacion", 2, 4),
+    FINAL_DOC(4, "Doc Final", 3, 0),
+    CANCELLATION(5, "Baja", -1, -1);
+
+    @Getter
+    private int id;
+    @Getter
+    private String name;
+    @Getter
+    private int previousId;
+    @Getter
+    private int nextId;
+
+    StateProcessEnum(int id, String name, int previousId, int nextId) {
+        this.id = id;
+        this.name = name;
+        this.previousId = previousId;
+        this.nextId = nextId;
+    }
+
 }
