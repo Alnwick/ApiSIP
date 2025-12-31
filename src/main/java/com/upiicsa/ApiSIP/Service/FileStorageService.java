9304c0b900725d1441eb0e7c4221c931ad6ed4de
Alnwick <Alnwick0809@outlook.com>
2025-12-30 18:49:39 -0600

    Clase para hacer el guardado del archivo en el directorio.

--- src/main/java/com/upiicsa/ApiSIP/Service/FileStorageService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/FileStorageService.java
@@ -1,4 +1,39 @@
 package com.upiicsa.ApiSIP.Service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
 public class FileStorageService {
+
+    private final Path root;
+
+    public FileStorageService(@Value("${storage.location}") String storagePath) {
+        this.root = Paths.get(storagePath).toAbsolutePath().normalize();
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("No se pudo inicializar el directorio de almacenamiento en " + root);
+        }
+    }
+
+    public void store(MultipartFile file, String fileName) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("Error al guardar archivo físico: " + fileName, e);
+        }
+    }
 }
