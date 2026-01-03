3d9d0ff813fe2fbf3bc47742e6e6bcc63eba8001
Alnwick <Alnwick0809@outlook.com>
2026-01-02 21:55:10 -0600

    Restructuracion del endpoint.

--- src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
+++ src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
@@ -4,7 +4,9 @@ import com.upiicsa.ApiSIP.Service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,11 +19,13 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
-    @RequestMapping("/upload")
-    @PreAuthorize("hasAnyRole('STUDENT')")
-    public ResponseEntity<Void> uploadDocument(MultipartFile file, String type) throws IOException {
+    @PostMapping("/upload")
+    @PreAuthorize("hasAnyRole('ALUMNO')")
+    public ResponseEntity<String> uploadDocument(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("type") String type) throws IOException {
         documentService.saveDoc(file, type);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("Uploaded successfully");
     }
 }
