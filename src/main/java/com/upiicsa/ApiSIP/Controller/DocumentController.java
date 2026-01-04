70a29434a501f158aa41c3f4fceae2ec114bd4b4
Alnwick <Alnwick0809@outlook.com>
2026-01-03 23:22:16 -0600

    Endpoint para mandar el estatus de documentos y se separo en otro metodo el obtener Id por Token.

--- src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
+++ src/main/java/com/upiicsa/ApiSIP/Controller/DocumentController.java
@@ -1,14 +1,15 @@
 package com.upiicsa.ApiSIP.Controller;
 
+import com.upiicsa.ApiSIP.Dto.Document.DocumentStatusDto;
 import com.upiicsa.ApiSIP.Service.DocumentService;
+import com.upiicsa.ApiSIP.Utils.AuthHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/documents")
@@ -17,13 +18,24 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
+    @GetMapping("/my-status")
+    @PreAuthorize("hasAnyRole('ALUMNO')")
+    public ResponseEntity<List<DocumentStatusDto>> getMyStatus(){
+
+        return ResponseEntity.ok(documentService.getStatus(getUserId()));
+    }
+
     @PostMapping("/upload")
     @PreAuthorize("hasAnyRole('ALUMNO')")
     public ResponseEntity<String> uploadDocument(
             @RequestParam("file") MultipartFile file,
             @RequestParam("type") String type){
 
-        documentService.saveDoc(file, type);
+        documentService.saveDoc(file, type, getUserId());
         return ResponseEntity.ok().body("Uploaded successfully");
+    }
+
+    private Integer getUserId(){
+        return AuthHelper.getAuthenticatedUserId();
     }
 }
