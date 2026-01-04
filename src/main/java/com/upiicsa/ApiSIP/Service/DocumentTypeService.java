75bbd6ef865300c67719f3e3c2e7e83e7f084c2a
Alnwick <Alnwick0809@outlook.com>
2026-01-03 23:19:05 -0600

    Clase service para lo tipos de documentos y metodo para obtener los documentos por estado de proceso.

--- src/main/java/com/upiicsa/ApiSIP/Service/DocumentTypeService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/DocumentTypeService.java
@@ -1,4 +1,25 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.DocumentType;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import com.upiicsa.ApiSIP.Repository.DocumentProcessRepository;
+import com.upiicsa.ApiSIP.Repository.DocumentTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DocumentTypeService {
+
+    @Autowired
+    private DocumentTypeRepository documentTypeRepository;
+
+    @Autowired
+    private DocumentProcessRepository docProcessRepository;
+
+    public List<DocumentType> getRequiredTypes(StudentProcess process){
+
+        return docProcessRepository.findDocumentTypesByProcessState(process.getProcessState());
+    }
 }
