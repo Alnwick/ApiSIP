1a1e1689ce3c5af1a69c952e119a84513a3e8ce4
Alnwick <Alnwick0809@outlook.com>
2026-01-08 14:23:16 -0600

    Metodo para generar un historial al actualizar estatus de alumno.

--- src/main/java/com/upiicsa/ApiSIP/Service/HistoryService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/HistoryService.java
@@ -1,4 +1,52 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Model.Catalogs.ProcessState;
+import com.upiicsa.ApiSIP.Model.Enum.StateProcessEnum;
+import com.upiicsa.ApiSIP.Model.History;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import com.upiicsa.ApiSIP.Model.UserSIP;
+import com.upiicsa.ApiSIP.Repository.HistoryRepository;
+import com.upiicsa.ApiSIP.Repository.ProcessStateRepository;
+import com.upiicsa.ApiSIP.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
 public class HistoryService {
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProcessStateRepository stateRepository;
+
+    @Transactional
+    public void saveHistory(StudentProcess process, StateProcessEnum newState, StateProcessEnum oldState) {
+        ProcessState newStateProcess = stateRepository.findByDescription(newState.getName())
+                .orElseThrow(()-> new RuntimeException("State not found"));
+        ProcessState oldStateProcess = stateRepository.findByDescription(oldState.getName())
+                .orElseThrow(()-> new RuntimeException("State not found"));
+
+        History newHistory = History.builder()
+                .process(process)
+                .user(getDefaultUser())
+                .updateDate(LocalDateTime.now())
+                .newState(newStateProcess)
+                .oldState(oldStateProcess).build();
+
+        historyRepository.save(newHistory);
+    }
+
+    private UserSIP getDefaultUser(){
+        UserSIP user = userRepository.findById(Integer.getInteger("100"))
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return user;
+    }
 }
