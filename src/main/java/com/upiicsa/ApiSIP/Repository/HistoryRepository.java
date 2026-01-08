8ef31565814eb3c9d3126c6d434de93137aa0ca5
Alnwick <Alnwick0809@outlook.com>
2026-01-08 14:25:29 -0600

    Clase repositorio para acceder al historial de la BD.

--- src/main/java/com/upiicsa/ApiSIP/Repository/HistoryRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/HistoryRepository.java
@@ -1,4 +1,14 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface HistoryRepository {
+import com.upiicsa.ApiSIP.Model.History;
+import com.upiicsa.ApiSIP.Model.StudentProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Integer> {
+
+    List<History> findByProcessOrderByUpdateDateAsc(StudentProcess process);
 }
