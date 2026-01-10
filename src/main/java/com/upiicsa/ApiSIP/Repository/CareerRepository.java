0a8d88c110dde506ef2ec964f2f6bef80583ab6b
Alnwick <Alnwick0809@outlook.com>
2026-01-09 18:15:18 -0600

    Clase repositorio para acceder a la informacion de la tabla de Carreras.

--- src/main/java/com/upiicsa/ApiSIP/Repository/CareerRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/CareerRepository.java
@@ -1,4 +1,14 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface CareerRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.Career;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CareerRepository extends JpaRepository<Career, Integer> {
+
+    Optional<Career> findCareerByAcronym(String acronym);
 }
