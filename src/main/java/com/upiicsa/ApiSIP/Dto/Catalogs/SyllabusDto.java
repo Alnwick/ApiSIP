91b1780a8d195dbe276a08b278fccc72ca2f2c86
Alnwick <Alnwick0809@outlook.com>
2026-01-09 18:13:54 -0600

    Clase DTO para mostrar la informacion de Planes de Estudios.

--- src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/SyllabusDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/SyllabusDto.java
@@ -1,6 +1,11 @@
-package com.upiicsa.ApiSIP.Dto;
+package com.upiicsa.ApiSIP.Dto.Catalogs;
+
+import com.upiicsa.ApiSIP.Model.Catalogs.Syllabus;
 
 public record SyllabusDto(
         String code
 ) {
+    public SyllabusDto(Syllabus s){
+        this(s.getCode());
+    }
 }
