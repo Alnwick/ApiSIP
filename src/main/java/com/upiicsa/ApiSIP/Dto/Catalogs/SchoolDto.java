0aee7cdfd8ef2f882fc6867b04fc64f259f8911b
Alnwick <Alnwick0809@outlook.com>
2026-01-09 18:13:09 -0600

    Clase DTO para mostrar la informacion de Escuelas.

--- src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/SchoolDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/SchoolDto.java
@@ -1,4 +1,12 @@
 package com.upiicsa.ApiSIP.Dto.Catalogs;
 
-public record SchoolDto() {
+import com.upiicsa.ApiSIP.Model.Catalogs.School;
+
+public record SchoolDto(
+        String name,
+        String acronym
+) {
+    public SchoolDto(School s) {
+        this(s.getName(), s.getAcronym());
+    }
 }
