f6ae937455ec5d27d38416d1ce2f137fcb185974
Alnwick <Alnwick0809@outlook.com>
2026-01-09 18:15:32 -0600

    Clase repositorio para acceder a la informacion de la tabla de Escuelas.

--- src/main/java/com/upiicsa/ApiSIP/Repository/SchoolRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/SchoolRepository.java
@@ -1,4 +1,13 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface SchoolRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+
+    Optional<School> findSchoolByAcronym(String acronym);
 }
