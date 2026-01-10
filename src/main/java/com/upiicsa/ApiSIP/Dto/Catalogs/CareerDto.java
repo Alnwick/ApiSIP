74c7bdc9f6c6b571c0c87196dc994bfe69f15cdb
Alnwick <Alnwick0809@outlook.com>
2026-01-09 18:12:54 -0600

    Clase DTO para mostrar la informacion de Carreras.

--- src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/CareerDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/Catalogs/CareerDto.java
@@ -1,7 +1,12 @@
-package com.upiicsa.ApiSIP.Dto;
+package com.upiicsa.ApiSIP.Dto.Catalogs;
+
+import com.upiicsa.ApiSIP.Model.Catalogs.Career;
 
 public record CareerDto(
         String name,
         String acronym
 ) {
+    public CareerDto(Career c){
+        this(c.getName(), c.getAcronym());
+    }
 }
