308b107b8ed001de9448a942633282b52feb7978
Alnwick <Alnwick0809@outlook.com>
2026-01-13 17:10:46 -0600

    Service para operadores.

--- src/main/java/com/upiicsa/ApiSIP/Service/OperativeService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/OperativeService.java
@@ -1,4 +1,44 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Dto.DashboardStatsDto;
+import com.upiicsa.ApiSIP.Model.Student;
+import com.upiicsa.ApiSIP.Repository.StudentProcessRepository;
+import com.upiicsa.ApiSIP.Repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
 public class OperativeService {
+
+    private StudentRepository studentRepository;
+
+    private StudentProcessRepository processRepository;
+
+    public DashboardStatsDto getStats(String careerAcronym){
+        List<Student> students;
+
+        if(careerAcronym.equals("all")){
+            students = studentRepository.findAll();
+        }
+        students = studentRepository.findAllByCareerAcronym(careerAcronym);
+
+        Map<String, Long> counts = students.stream()
+                .map(student -> processRepository.findByStudentId(student.getId()).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(
+                        process -> process.getProcessState().getDescription(),
+                        Collectors.counting()
+                ));
+
+        return new DashboardStatsDto(
+                students.size(),
+                counts.getOrDefault("Registrado", 0L).intValue(),
+                counts.getOrDefault("Doc Inicial", 0L).intValue(),
+                counts.getOrDefault("Carta Aceptacion", 0L).intValue(),
+                counts.getOrDefault("Doc Final", 0L).intValue()
+        );
+    }
+
 }
