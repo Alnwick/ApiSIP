f73544762a55638dab32456b996a50acbb407af1
Alnwick <Alnwick0809@outlook.com>
2026-01-13 17:10:04 -0600

    Metodo para obtener las stats por en la pagina.

--- src/main/java/com/upiicsa/ApiSIP/Controller/OperativeController.java
+++ src/main/java/com/upiicsa/ApiSIP/Controller/OperativeController.java
@@ -1,21 +1,29 @@
 package com.upiicsa.ApiSIP.Controller;
 
+import com.upiicsa.ApiSIP.Dto.DashboardStatsDto;
 import com.upiicsa.ApiSIP.Model.Student;
+import com.upiicsa.ApiSIP.Service.OperativeService;
 import com.upiicsa.ApiSIP.Service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/operatives")
 public class OperativeController {
 
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
+
+    private final OperativeService operativeService;
+
+    public OperativeController(StudentService studentService, OperativeService operativeService) {
+        this.studentService = studentService;
+        this.operativeService = operativeService;
+    }
 
     @RequestMapping("/get-allStudents")
     @PreAuthorize("hasAnyRole('ADMIN', 'OPERATIVE')")
@@ -23,5 +31,11 @@ public class OperativeController {
         Page<Student> students = studentService.getStudents(pageable);
 
         return ResponseEntity.ok(students);
+    }
+
+    @RequestMapping("/stats")
+    @PreAuthorize("hasAnyRole('ADMIN', 'OPERATIVE')")
+    public ResponseEntity<DashboardStatsDto> getStats(@RequestParam String careerAcronym) {
+        return ResponseEntity.ok(operativeService.getStats(careerAcronym));
     }
 }
