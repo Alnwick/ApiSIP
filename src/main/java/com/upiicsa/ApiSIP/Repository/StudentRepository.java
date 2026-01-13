964f4184b8d04ef267b714f452b1c2184d8b04b6
Alnwick <Alnwick0809@outlook.com>
2026-01-13 17:11:47 -0600

    Metodo para obtener estudiantes por carrera.

--- src/main/java/com/upiicsa/ApiSIP/Repository/StudentRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/StudentRepository.java
@@ -2,9 +2,15 @@ package com.upiicsa.ApiSIP.Repository;
 
 import com.upiicsa.ApiSIP.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    @Query("SELECT s FROM Student s JOIN s.offer o JOIN o.career c WHERE c.acronym = :acronym")
+    List<Student> findAllByCareerAcronym(@Param("acronym") String acronym);
 }
