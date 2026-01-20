36f0fabbcc3a005c3818340f877e49480c7b4093
Alnwick <Alnwick0809@outlook.com>
2026-01-20 13:57:23 -0600

    Cambio

--- src/main/java/com/upiicsa/ApiSIP/PasswordHashingTest.java
+++ src/main/java/com/upiicsa/ApiSIP/PasswordHashingTest.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PasswordHashingTest {
     public static void main(String[] args) {
-        String password = "Yatg0809";
+        String password = "SpringCreator";
 
         // Crea el codificador de contraseñas usando BCrypt
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
