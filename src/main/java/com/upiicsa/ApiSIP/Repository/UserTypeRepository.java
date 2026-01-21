81a14b104eb564bd777efe44f9273c72937a2bee
Alnwick <Alnwick0809@outlook.com>
2026-01-20 19:13:45 -0600

    Cambio de nombre.

--- src/main/java/com/upiicsa/ApiSIP/Repository/UserTypeRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/UserTypeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TypeUserRepository extends JpaRepository<UserType, Integer> {
+public interface UserTypeRepository extends JpaRepository<UserType, Integer> {
 
     Optional<UserType> findByDescription(String description);
 }
