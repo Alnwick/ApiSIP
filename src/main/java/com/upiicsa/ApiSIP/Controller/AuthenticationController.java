2520efe8e550536560fd412c7506d150a5a5ddb6
Alnwick <Alnwick0809@outlook.com>
2026-01-22 14:08:55 -0600

    Refactorizacion quitando @Autowired y poniendo constructor

--- src/main/java/com/upiicsa/ApiSIP/Controller/AuthenticationController.java
+++ src/main/java/com/upiicsa/ApiSIP/Controller/AuthenticationController.java
@@ -8,7 +8,6 @@ import com.upiicsa.ApiSIP.Utils.JwtUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,14 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @Autowired
     private AuthenticationManager authenticationManager;
-
-    @Autowired
     private JwtUtils jwtUtils;
-
-    @Autowired
     private UserRepository userRepository;
+
+    public AuthenticationController(AuthenticationManager authenticationManager,  JwtUtils jwtUtils,
+                                    UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+        this.userRepository = userRepository;
+    }
 
 
     @PostMapping("/login")
