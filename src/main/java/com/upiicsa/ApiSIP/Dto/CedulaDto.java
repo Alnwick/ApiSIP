50e0adaa95118883b347e185c3a4fd4910cdb5e3
Alnwick <Alnwick0809@outlook.com>
2026-01-23 14:33:15 -0600

    Clase DTO para mandar toda la informacion de la cedula.

--- src/main/java/com/upiicsa/ApiSIP/Dto/CedulaDto.java
+++ src/main/java/com/upiicsa/ApiSIP/Dto/CedulaDto.java
@@ -1,4 +1,8 @@
 package com.upiicsa.ApiSIP.Dto;
 
-public record CedulaDto() {
+public record CedulaDto(
+        AddressDto studentAddress,
+        CompanyDto companyInfo,
+        AddressDto companyAddress
+) {
 }
