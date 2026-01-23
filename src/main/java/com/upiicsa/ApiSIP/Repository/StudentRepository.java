9373471623ef58ca280f016fdb4361757644e29a
Alnwick <Alnwick0809@outlook.com>
2026-01-23 14:34:58 -0600

    New: Metodo para buscar un estudiante por su ID.

--- src/main/java/com/upiicsa/ApiSIP/Repository/StudentRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/StudentRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Optional<Student> findById(int id);
 
     @Query("SELECT s FROM Student s JOIN s.offer o JOIN o.career c WHERE c.acronym = :acronym")
     List<Student> findAllByCareerAcronym(@Param("acronym") String acronym);
