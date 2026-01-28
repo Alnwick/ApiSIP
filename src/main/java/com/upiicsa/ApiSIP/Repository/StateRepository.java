988486f72d02dcbd058a46aaf5ec193ec5b13838
Alnwick <Alnwick0809@outlook.com>
2026-01-27 18:17:42 -0600

    Repositorio.

--- src/main/java/com/upiicsa/ApiSIP/Repository/StateRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/StateRepository.java
@@ -1,4 +1,12 @@
 package com.upiicsa.ApiSIP.Repository;
 
-public interface StateRepository {
+import com.upiicsa.ApiSIP.Model.Catalogs.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StateRepository extends JpaRepository<State,Integer> {
+    Optional<State> findById(Integer id);
 }
