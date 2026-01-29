2f952f977dc517ff4cf811dd5c9a29b52bf2602e
Alnwick <Alnwick0809@outlook.com>
2026-01-28 18:00:23 -0600

    Metodos para crear direccion y actualizar.

--- src/main/java/com/upiicsa/ApiSIP/Service/AddressService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/AddressService.java
@@ -1,4 +1,64 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Dto.AddressDto;
+import com.upiicsa.ApiSIP.Model.Address;
+import com.upiicsa.ApiSIP.Model.Catalogs.State;
+import com.upiicsa.ApiSIP.Repository.AddressRepository;
+import com.upiicsa.ApiSIP.Repository.StateRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AddressService {
+
+    private AddressRepository addressRepository;
+    private StateRepository stateRepository;
+
+    public AddressService(AddressRepository addressRepository, StateRepository stateRepository) {
+        this.addressRepository = addressRepository;
+        this.stateRepository = stateRepository;
+    }
+
+    @Transactional
+    public Address createAddress(AddressDto addressDto) {
+        State state = stateRepository.findById(addressDto.stateId())
+                .orElseThrow(() -> new EntityNotFoundException("State not found"));
+
+        Address newAddress = Address.builder()
+                .street(addressDto.street())
+                .number(addressDto.number())
+                .neighborhood(addressDto.neighborhood())
+                .zipCode(addressDto.zipCode())
+                .state(state)
+                .build();
+
+        addressRepository.save(newAddress);
+        return newAddress;
+    }
+
+    @Transactional
+    public Address updateAddress(Integer addressId, AddressDto addressDto) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new EntityNotFoundException("Address not found"));
+
+        if(!addressDto.street().equals(address.getStreet())){
+            address.setStreet(addressDto.street());
+        }
+        if(!addressDto.number().equals(address.getNumber())){
+            address.setNumber(addressDto.number());
+        }
+        if(!addressDto.zipCode().equals(address.getZipCode())){
+            address.setZipCode(addressDto.zipCode());
+        }
+        if(!addressDto.neighborhood().equals(address.getNeighborhood())){
+            address.setNeighborhood(addressDto.neighborhood());
+        }
+        if(!addressDto.stateId().equals(address.getState().getId())){
+            State state = stateRepository.findById(addressDto.stateId())
+                    .orElseThrow(() -> new EntityNotFoundException("State not found"));
+            address.setState(state);
+        }
+        return addressRepository.save(address);
+    }
 }
