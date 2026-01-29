fbc2b2362688c785f189a4ec458aad3edd558202
Alnwick <Alnwick0809@outlook.com>
2026-01-28 18:01:46 -0600

    Metodos para poder crear y actualizar empresa.

--- src/main/java/com/upiicsa/ApiSIP/Repository/CompanyRepository.java
+++ src/main/java/com/upiicsa/ApiSIP/Repository/CompanyRepository.java
@@ -4,8 +4,11 @@ import com.upiicsa.ApiSIP.Model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Integer> {
 
+    Optional<Company> findById(Integer id);
 }
--- src/main/java/com/upiicsa/ApiSIP/Service/CompanyService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/CompanyService.java
@@ -1,4 +1,72 @@
 package com.upiicsa.ApiSIP.Service;
 
+import com.upiicsa.ApiSIP.Dto.CompanyDto;
+import com.upiicsa.ApiSIP.Model.Address;
+import com.upiicsa.ApiSIP.Model.Company;
+import com.upiicsa.ApiSIP.Repository.CompanyRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Transactional
+    public Company createCompany(CompanyDto companyDto, Address address) {
+        Company company = Company.builder()
+                .name(companyDto.name()).email(companyDto.email())
+                .sector(companyDto.sector()).phone(companyDto.phone())
+                .extension(companyDto.extension()).fax(companyDto.fax())
+                .supervisor(companyDto.supervisor()).supervisorGrade(companyDto.supervisorGrade())
+                .positionSupervisor(companyDto.positionSupervisor()).positionStudent(companyDto.positionStudent())
+                .address(address)
+                .build();
+
+        return companyRepository.save(company);
+    }
+
+    @Transactional
+    public Company updateCompany(Integer companyId, CompanyDto companyDto, Address address) {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new EntityNotFoundException("Company not found"));
+
+        if(!companyDto.name().equals(company.getName())) {
+            company.setName(companyDto.name());
+        }
+        if(!companyDto.email().equals(company.getEmail())) {
+            company.setEmail(companyDto.email());
+        }
+        if(!companyDto.sector().equals(company.getSector())) {
+            company.setSector(companyDto.sector());
+        }
+        if(!companyDto.phone().equals(company.getPhone())) {
+            company.setPhone(companyDto.phone());
+        }
+        if(!companyDto.extension().equals(company.getExtension())) {
+            company.setExtension(company.getExtension());
+        }
+        if(!companyDto.fax().equals(company.getFax())) {
+            company.setFax(company.getFax());
+        }
+        if(!companyDto.supervisor().equals(company.getSupervisor())) {
+            company.setSupervisor(company.getSupervisor());
+        }
+        if(!companyDto.supervisorGrade().equals(company.getSupervisorGrade())) {
+            company.setSupervisorGrade(company.getSupervisorGrade());
+        }
+        if(!companyDto.positionSupervisor().equals(company.getPositionSupervisor())) {
+            company.setPositionSupervisor(company.getPositionSupervisor());
+        }
+        if(!companyDto.positionStudent().equals(company.getPositionStudent())) {
+            company.setPositionStudent(company.getPositionStudent());
+        }
+        company.setAddress(address);
+        return companyRepository.save(company);
+    }
 }
