35a4860ad523d0dfb65e24eed6a0c40abd1525ab
Yael Alejandro Trujillo Godoy <Alnwick0809@outlook.com>
2026-03-20 21:38:09 -0600

    Cambios
    Cambios de UserRepository a UserService y ya no se usan los estado del Enum, si no de BD.

--- src/main/java/com/upiicsa/ApiSIP/Service/HistoryService.java
+++ src/main/java/com/upiicsa/ApiSIP/Service/HistoryService.java
@@ -1,14 +1,10 @@
 package com.upiicsa.ApiSIP.Service;
 
 import com.upiicsa.ApiSIP.Model.Catalogs.ProcessStatus;
-import com.upiicsa.ApiSIP.Model.Enum.StateProcessEnum;
 import com.upiicsa.ApiSIP.Model.History;
 import com.upiicsa.ApiSIP.Model.Document_Process.StudentProcess;
 import com.upiicsa.ApiSIP.Model.UserSIP;
 import com.upiicsa.ApiSIP.Repository.HistoryRepository;
-import com.upiicsa.ApiSIP.Repository.Catalogs.ProcessStatusRepository;
-import com.upiicsa.ApiSIP.Repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,41 +18,34 @@ public class HistoryService {
     @Value("${default.user}")
     private Integer defaultUser;
     private HistoryRepository historyRepository;
-    private UserRepository userRepository;
-    private ProcessStatusRepository stateRepository;
+    private UserService userService;
 
-    public HistoryService(HistoryRepository historyRepository, UserRepository userRepository,
-                          ProcessStatusRepository stateRepository) {
+    public HistoryService(HistoryRepository historyRepository, UserService userService) {
         this.historyRepository = historyRepository;
-        this.userRepository = userRepository;
-        this.stateRepository = stateRepository;
+        this.userService = userService;
     }
 
     @Transactional(readOnly = true)
     public List<History> getHistoriesByProcess(StudentProcess process){
-        return  historyRepository.findByProcessOrderByUpdateDateAsc(process);
+        return historyRepository.findByProcessOrderByUpdateDateAsc(process);
     }
 
     @Transactional
-    public void saveHistory(StudentProcess process, StateProcessEnum oldState, StateProcessEnum newState) {
-        ProcessStatus newStateProcess = stateRepository.findByDescription(newState.getName())
-                .orElseThrow(()-> new EntityNotFoundException("State not found"));
-        ProcessStatus oldStateProcess = stateRepository.findByDescription(oldState.getName())
-                .orElseThrow(()-> new EntityNotFoundException("State not found"));
+    public void saveHistory(StudentProcess process, ProcessStatus oldStatus, ProcessStatus newStatus) {
 
         History newHistory = History.builder()
                 .process(process)
                 .user(getDefaultUser())
                 .updateDate(LocalDateTime.now())
-                .newState(newStateProcess)
-                .oldState(oldStateProcess)
+                .newState(oldStatus)
+                .oldState(newStatus)
                 .build();
 
         historyRepository.save(newHistory);
     }
 
     private UserSIP getDefaultUser(){
-        UserSIP user = userRepository.findById(defaultUser)
+        UserSIP user = userService.getUserById(defaultUser)
                 .orElse(null);
         return user;
     }
